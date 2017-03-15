844017d49f5126b2ab51e101ec63a093ea70ceb1
Tomas Dohnalek <tomas.dohnalek@firma.seznam.cz>
2017-03-15 21:53:46 +0100

    chore: 0.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pydor',
-    version='0.1.0',
+    version='0.2.0',
     description='Python docker registry (distribution) client',
     long_description=long_description,
     url='https://github.com/dohnto/pydor',
