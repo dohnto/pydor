1ed279311007646a1d9850f28cb2eceff376e4cb
Tomas Dohnalek <dohnto@gmail.com>
2017-03-16 22:01:06 +0100

    feat: ping feature

--- pydor/errors.py
+++ pydor/errors.py
@@ -1,3 +1,8 @@
 class EntityNotFound(ValueError): pass
 class TagNotFound(EntityNotFound): pass
 class RepoNotFound(EntityNotFound): pass
+
+SSL_ERROR_CODE = 1
+ENTITY_NOT_FOUND = 2
+NOT_V2_REGISTRY = 3
+NOT_IMPLEMENTED = 255
