4379f20f3b9f1b6c0b43384059315abaf5aa9613
Tomas Dohnalek <dohnto@gmail.com>
2017-03-16 21:39:22 +0100

    test: added reproducer for #1

--- test/commandline_test.py
+++ test/commandline_test.py
@@ -3,6 +3,7 @@ import pydor.commandline
 import unittest
 import click.testing
 import requests_mock
+import requests
 
 
 class TestCommandline(unittest.TestCase):
@@ -20,7 +21,6 @@ class TestCommandline(unittest.TestCase):
 
     @requests_mock.mock()
     def test_tags_empty(self, m):
-        api = pydor.api.API('registry.test')
         with open("test/mock/manifest/list_tags_empty.json") as mock_response_file:
             mock_response = mock_response_file.read()
             m.get('https://registry.test/v2/generic/foo/tags/list', text=mock_response)
@@ -29,3 +29,15 @@ class TestCommandline(unittest.TestCase):
             result = runner.invoke(pydor.commandline.tags, ["--output", "json", "registry.test/generic/foo"])
             self.assertEqual(result.exit_code, 0)
             self.assertEqual(result.output, "[]\n")
+
+    @requests_mock.mock()
+    def test_list_non_registry(self, m):
+        # issue https://github.com/dohnto/pydor/issues/1
+        m.get("https://quay.io/quay/elasticsearch/v2/_catalog", status_code=requests.codes.not_found)
+
+        runner = click.testing.CliRunner()
+        result = runner.invoke(pydor.commandline.list, [ "quay.io/quay/elasticsearch"])
+        self.assertEqual(result.exit_code, -1)
+        self.assertEqual(result.output, "[]\n")
+
+
