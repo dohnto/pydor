504543e82bd81040e97114b51ce9b562b6d39af5
Tomas Dohnalek <tomas.dohnalek@firma.seznam.cz>
2017-04-29 22:49:37 +0200

    chore: version bump 0.2.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pydor',
-    version='0.2.2',
+    version='0.2.3',
     description='Python docker registry (distribution) client',
     long_description=long_description,
     url='https://github.com/dohnto/pydor',
